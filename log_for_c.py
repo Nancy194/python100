4b53af91886e785309bf133e116d65baa943c95e
nancy.dai <nancy.dai@uxin.com>
2017-04-10 17:35:30 +0800

    mac

--- log_for_c.py
+++ log_for_c.py
@@ -1,16 +1,30 @@
 # coding=utf-8
 
-start='''    LOGD("++%s",__func__); \n'''
-last='''     LOGD("--%s",__func__);	\n	'''
+startstring='''    LOGD("++%s",__func__); \n'''
+returnstring='''     LOGD("--%s",__func__);	\n	'''
 
 list=['int','int8_t','int16_t','int32_t',
-      'void','fp_image_t*','static',
+      'void','fp_image_t','fp_image_t*','static','struct',
       'uint','uint8_t','uint16_t','uint32_t',
       'int*','int8_t*','int16_t*','int32_t*',
       'uint*','uint8_t*','uint16_t*','uint32_t*']
 
 s1='('
 s2='{'
+testlist=['qwe','rty','uio','asd']
+
+
+def remove_duplicate(list):
+    ''''
+    去重，如果一个数组临近的两个元素一摸一样，则去掉其中一个
+    '''
+    l=list
+    le=len(list)
+    for i in range(le-1):
+        if list[i]==list[i+1]:
+            l=l[:i]+l[i+1:]
+    return l
+
 
 def find_s(string,s):
     '''
@@ -25,12 +39,8 @@ def find_s(string,s):
     return 0
 
 
-
-def find_func_start_location():
-    f=open('/Users/nancy/Documents/个人学习资料/python_loging/testfile.c','r+')
+def start_location(alllist):
     linenumber=0
-    alllist=f.readlines()
-
     for everyline in alllist:             # 遍历每一行
         a=everyline.split(' ')    # a是一行根据空格分割而成的字符串数组
         linenumber+=1
@@ -41,23 +51,82 @@ def find_func_start_location():
                      if find_s(alllist[linenumber+i-1],s2):
                          l=linenumber+i
                          break
-                for j in range(30):       # 在大括号那一行开始开始检查每一行最左边的字符是否在list里面，如果不在则停止查询
+                for j in range(30):     # 在大括号那行开始检查每行最左边的非空格字符是否在list里面，如不在则停止查询，插入log
                     b=alllist[l+j].strip().split(' ')
                     if len(b)>0:
                         if b[0] not in list:
                             l=l+j
                             break
+                alllist.insert(l, startstring)     # 插入开始调用函数的log打印语句
 
-                alllist.insert(l, start)     # 插入开始调用函数的log打印语句
-
+    return alllist
 #    for i in alllist:
 #        print(i,end='')
-    lines=''.join(alllist)
+#    lines=''.join(alllist)
+#    f.write(lines)
+#    f.write('/*--------------------end--------------------*/')
+
+def end_location(alllist):
+    '''
+    添加函数结束log，但是log在return之后
+    :param alllist:
+    :return:
+    '''
+    linenumber=0
+    for s in alllist:
+        linenumber+=1
+        s1=s.strip().split(' ')
+        if s1[0]=='return':
+            alllist.insert(linenumber,returnstring)
+    return alllist
+#    for i in alllist:
+#        print(i,end='')
+
+
+def change_return(list,n):
+    '''
+    交换list的n与n+1行
+    :param list:
+    :param n:
+    :return:
+    '''
+    list[n],list[n+1]=list[n+1],list[n]
+
+
+def find_all_return(list):
+    '''
+    找出所有return的行数
+    :param list:
+    :return:
+    '''
+    n=[]
+    l=0
+    for s in list:
+        l+=1
+        if s.strip().split(' ')[0]=='return':
+            n.append(l-1)
+    return n
+
+def avoid_duplication(list):
+    
+
+
+if __name__ == '__main__':
+    f = open('/Users/nancy/Documents/个人学习资料/python_loging/testop.c', 'r+')
+    al = f.readlines()
+    sta=start_location(al)
+    en=end_location(sta)
+#    print(en)
+#    print(find_all_return(en))
+    for i in find_all_return(en):
+        change_return(en,i)
+#    for i in en:
+#        print(i,end='')
+    f.seek(0,0)
+    lines = ''.join(en)
     f.write(lines)
-    f.write('/*------------------------------------------------------------------------*/')
+    f.close()
 
-
-find_func_start_location()
 
 
 
